078fab7c8d17cd7cbffede502f64ece7b5966f7a
Dilnaz <dilnaz.m13@gmail.com>
2025-04-18 13:24:51 +0500

    kall

--- client/src/components/UserLogin.jsx
+++ client/src/components/UserLogin.jsx
@@ -1,14 +1,15 @@
+import React from "react";
 import { SignIn } from "@clerk/clerk-react";
 
 const UserLogin = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-50">
-      <SignIn
+        <SignIn
         path="/sign-in"
         routing="path"
         signUpUrl="/sign-up"
         redirectUrl="/"
-      />
+        />
     </div>
   );
 };
